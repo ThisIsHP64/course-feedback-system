05b8a7fcd36b2191ab59b72d427329e131b74154
jlafrance3 <jlafranceiii@proton.me>
2025-11-25 13:54:10 -0500

    Added FeebackCard react component

---
+++ src/components/FeedbackCard.tsx
@@ -0,0 +1,31 @@
+/**
+ * FeedbackCard.tsx
+ * Card component to display lesson feedback and star rating
+ */
+
+import { Card } from "react-bootstrap";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+type CardProps = {
+  comment: string;
+  rating: number;
+  date: Date;
+};
+
+function FeedbackCard({ comment, rating, date }: CardProps) {
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Title>Feedback</Card.Title>
+        <div className="mb-2">
+          <Rating value={rating} readOnly />
+        </div>
+        <Card.Text>{comment}</Card.Text>
+      </Card.Body>
+      <Card.Footer>{new Date(date).toLocaleDateString()}</Card.Footer>
+    </Card>
+  );
+}
+
+export default FeedbackCard;
---
+++ src/components/WeekLessonsCard.tsx
@@ -0,0 +1,5 @@
+/**
+ * WeekLessonsCard.tsx
+ * Used to group lessons by week, a card that displays
+ * the week number and the lessons passed as props.
+ */
