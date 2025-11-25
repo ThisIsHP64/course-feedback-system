3f315b21b3321cf38c49ef818dcee5d7565fedaf
jlafrance3 <jlafranceiii@proton.me>
2025-11-25 13:13:34 -0500

    Added sample data json

---
+++ src/types/types.ts
@@ -0,0 +1,35 @@
+// Users
+export interface User {
+  id: number;
+  name: string;
+  role: "student" | "professor";
+}
+
+// Course Info
+export interface Course {
+  id: number;
+  code: string;
+  name: string;
+  professorId: string;
+  description: string;
+}
+
+// Lesson Info
+export interface Lesson {
+  id: number;
+  courseId: number;
+  title: string;
+  date: string;
+  description: string;
+  week: number;
+}
+
+// Feedback
+export interface Feedback {
+  id: number;
+  lessonId: number;
+  studentId: number;
+  rating: number;
+  comment: string;
+  submittedAt: string;
+}
