4f77e90c574916167d3fee24a6f0299ed181e61c
jlafrance3 <jlafranceiii@proton.me>
2025-11-25 18:16:22 -0500

    Added LogFeedback page

---
+++ src/pages/LessonList.tsx
@@ -0,0 +1,5 @@
+/**
+ * LessonList.tsx
+ * Page component that displays lesson information. Some page features will be
+ * conditionally rendered depending whether the user is a student or a professor.
+ */
---
+++ src/pages/ViewFeedback.tsx
@@ -0,0 +1,5 @@
+/**
+ * ViewFeedback.tsx
+ * Page component that allows professor users to view a list of feedback for a
+ * specific lesson.
+ */
